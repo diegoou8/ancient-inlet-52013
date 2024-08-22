164f4bc5ddcc57db2f0e477e71eaac05f5f11a6c
diegoou8 <diegoh@ou.com>
2024-08-22 16:23:42 -0500

    Discount Aug2

--- server.js
+++ server.js
@@ -47,7 +47,7 @@ app.post('/shipping', (request, response) => {
                     method: "Envío Bogotá",
                     price: 0,
                     service_id: 10001,
-                    service_name: "Envío Bogotá (24 – 48 Horas)",
+                    service_name: "Envío Gratis Bogotá (24 – 48 Horas)",
                 });
 
                 // Priority Bogotá Shipping (conditional based on time)
